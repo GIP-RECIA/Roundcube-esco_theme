ba609ceb3cf27122c792a60c3cb52ffb205b7171
gbrousse-recia <gregory.brousse@recia.fr>
2023-04-18 10:21:37 +0200

    fix: iframe handle

--- templates/includes/extended-uportal/scripts.php
+++ templates/includes/extended-uportal/scripts.php
@@ -3,13 +3,10 @@
 $(()=>{
   if ( self !== top ) {
     console.debug('Roundcube theme : in iframe');
-    $('#esco-header').hide();
-    $('#esco-footer').hide();
+    $('#layout').addClass('in-iframe');
   }else{
     const date = new Date();
     const cacheBuster = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`;
-    $('#esco-header').show();
-    $('#esco-footer').show();
     $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-header.min.js?c="+cacheBuster).appendTo("body");
     $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-footer.min.js?c="+cacheBuster).appendTo("body");
   }
