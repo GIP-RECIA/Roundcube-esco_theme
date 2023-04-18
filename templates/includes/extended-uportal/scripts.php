291714839e91ae03d1e7b36557fab3a04da9c213
gbrousse-recia <gregory.brousse@recia.fr>
2023-04-18 10:05:34 +0200

    feat: handle iframe

--- templates/includes/extended-uportal/scripts.php
+++ templates/includes/extended-uportal/scripts.php
@@ -1,9 +1,17 @@
 
 <script type="text/javascript" defer> 
 $(()=>{
-  const date = new Date();
-  const cacheBuster = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`;
-  $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-header.min.js?c="+cacheBuster).appendTo("body");
-  $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-footer.min.js?c="+cacheBuster).appendTo("body");
+  if ( self !== top ) {
+    console.debug('Roundcube theme : in iframe');
+    $('#esco-header').hide();
+    $('#esco-footer').hide();
+  }else{
+    const date = new Date();
+    const cacheBuster = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`;
+    $('#esco-header').show();
+    $('#esco-footer').show();
+    $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-header.min.js?c="+cacheBuster).appendTo("body");
+    $("<script>").attr("type","text/javascript").attr("src","/commun/extended-uportal-footer.min.js?c="+cacheBuster).appendTo("body");
+  }
 });
 </script>
